1bc708f27ade87eb471ec3b86e096b68ea596e94
lgodleski <77468991+lgodleski@users.noreply.github.com>
2021-04-11 18:00:58 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -20,7 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemispheres": hemispheres(browser)
+        "hemispheres": hemisphere_image_url
     }
 
     # Stop webdriver and return data
